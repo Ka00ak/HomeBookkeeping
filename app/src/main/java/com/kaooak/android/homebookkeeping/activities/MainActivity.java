1969d629ac867120a23d733756b806db01a70c1d
Ka00ak <kao.oak@yandex.ru>
2018-08-22 22:04:25 +0700

    Update MainActivity to Spinner with LoaderCallbacks<Cursor>

--- app/src/main/java/com/kaooak/android/homebookkeeping/activities/MainActivity.java
+++ app/src/main/java/com/kaooak/android/homebookkeeping/activities/MainActivity.java
@@ -1,87 +1,101 @@
 package com.kaooak.android.homebookkeeping.activities;
 
-import android.content.Intent;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.kaooak.android.homebookkeeping.R;
 import com.kaooak.android.homebookkeeping.ValuesSingleton;
 import com.kaooak.android.homebookkeeping.data.JSON.GsonData;
 import com.kaooak.android.homebookkeeping.data.RetrofitCB;
+import com.kaooak.android.homebookkeeping.database.DbContract;
 import com.kaooak.android.homebookkeeping.fragments.AccountsListFragment;
-import com.kaooak.android.homebookkeeping.fragments.TransactionsListFragment;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
 
     private int currentFragment;
 
+    private SimpleCursorAdapter mSimpleCursorAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.test_activity_accounts);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        getSupportLoaderManager().initLoader(0, null ,this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_accounts);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+        actionBar.setDisplayShowTitleEnabled(false);
 
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-        mNavigationView = findViewById(R.id.navigation_view);
-        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                item.setChecked(true);
+        mSimpleCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_spinner_item, null, new String[] { "name"}, new int[] { android.R.id.text1}, 0);
+        mSimpleCursorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner = toolbar.findViewById(R.id.spinner_accounts);
+        spinner.setAdapter(mSimpleCursorAdapter);
 
-                mDrawerLayout.closeDrawers();
+//        mDrawerLayout = findViewById(R.id.drawer_layout);
+//        mNavigationView = findViewById(R.id.navigation_view);
+//        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                item.setChecked(true);
+//
+//                mDrawerLayout.closeDrawers();
+//
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                switch (item.getItemId()) {
+//                    case R.id.menu_drawer_item_accounts:
+//                        setTitle("Счета");
+//
+//                        fragmentManager.beginTransaction()
+//                                .replace(R.id.container_fragment, AccountsListFragment.newInstance())
+//                                .commit();
+//                        return true;
+//                    case R.id.menu_drawer_item_transactions:
+//                        setTitle("Транзакции");
+//
+//                        fragmentManager.beginTransaction()
+//                                .replace(R.id.container_fragment, TransactionsListFragment.newInstance())
+//                                .commit();
+//                        return true;
+//                    default:
+//                        return false;
+//                }
+//            }
+//        });
 
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                switch (item.getItemId()) {
-                    case R.id.menu_drawer_item_accounts:
-                        setTitle("Счета");
-
-                        fragmentManager.beginTransaction()
-                                .replace(R.id.container_fragment, AccountsListFragment.newInstance())
-                                .commit();
-                        return true;
-                    case R.id.menu_drawer_item_transactions:
-                        setTitle("Транзакции");
-
-                        fragmentManager.beginTransaction()
-                                .replace(R.id.container_fragment, TransactionsListFragment.newInstance())
-                                .commit();
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-        });
-
-        new RetrofitAsyncTask().execute("");
-
-        currentFragment = 0;
-        setTitle("Счета");
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container_fragment, AccountsListFragment.newInstance())
-                .commit();
+//        new RetrofitAsyncTask().execute("");
+//
+//        currentFragment = 0;
+//        setTitle("Счета");
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        fragmentManager.beginTransaction()
+//                .replace(R.id.container_fragment, AccountsListFragment.newInstance())
+//                .commit();
     }
 
     @Override
@@ -94,6 +108,24 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    //
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return new CursorLoader(this, DbContract.AccountsTable.CONTENT_URI, null, null, null,null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        mSimpleCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        mSimpleCursorAdapter.swapCursor(null);
+    }
+    //
 
 //    @Override
 //    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
